4f208af3bf5d8f76aa52bc0f87ba581836134be0
wanxinlei1 <wanxinlei1@users.noreply.github.com>
2026-05-16 20:24:06 +0800

    "Remove_build_artifacts_and_crash_logs"

--- _build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set JAVA_HOME=C:\Program Files\Zulu\zulu-17
-set PATH=%JAVA_HOME%\bin;%PATH%
-cd /d C:\Users\wxl\Desktop\chat
-call gradlew.bat assembleDebug --no-daemon --warning-mode=none 2>&1
